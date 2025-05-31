ab1f0bbe64a566aca6a8fc14d37634e60cb1efeb
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-31 23:01:24 +0100

    add WhatExecLib Extensions

---
+++ src/WhatExecLib.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WhatExecLib.Abstractions;
+
+namespace WhatExecLib.Extensions.DependencyInjection;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddWhatExecLib(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    {
+        switch (lifetime)
+        {
+            case ServiceLifetime.Scoped:
+                services.AddScoped<IDirectoryListPrioritizer, DirectoryListPrioritizer>();
+                    
+                services.AddScoped<IExecutableFileDetector, ExecutableFileDetector>();
+                services.AddScoped<IExecutableFileLocator, ExecutableFileLocator>();
+                services.AddScoped<IExecutableFileInstancesLocator, ExecutableFileInstancesLocator>();
+                services.AddScoped<IMassExecutableLocator, MassExecutableLocator>();
+                services.AddScoped<IPrioritizedExecutableFileLocator, PrioritizedExecutableFileLocator>();
+                break;
+            case ServiceLifetime.Singleton:
+                services.AddSingleton<IDirectoryListPrioritizer, DirectoryListPrioritizer>();
+                    
+                services.AddSingleton<IExecutableFileDetector, ExecutableFileDetector>();
+                services.AddSingleton<IExecutableFileLocator, ExecutableFileLocator>();
+                services.AddSingleton<IExecutableFileInstancesLocator, ExecutableFileInstancesLocator>();
+                services.AddSingleton<IMassExecutableLocator, MassExecutableLocator>();
+                services.AddSingleton<IPrioritizedExecutableFileLocator, PrioritizedExecutableFileLocator>();
+                break;
+            case ServiceLifetime.Transient:
+                services.AddTransient<IDirectoryListPrioritizer, DirectoryListPrioritizer>();
+                    
+                services.AddTransient<IExecutableFileDetector, ExecutableFileDetector>();
+                services.AddTransient<IExecutableFileLocator, ExecutableFileLocator>();
+                services.AddTransient<IExecutableFileInstancesLocator, ExecutableFileInstancesLocator>();
+                services.AddTransient<IMassExecutableLocator, MassExecutableLocator>();
+                services.AddTransient<IPrioritizedExecutableFileLocator, PrioritizedExecutableFileLocator>();
+                break;
+        }
+            
+        return services;
+    }
+}
