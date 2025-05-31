f921694b5a663022747e34a475dd41879e4757e2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-31 22:32:47 +0100

    Create IExecutableFileLocator.cs

---
+++ src/WhatExecLib/Abstractions/IExecutableFileLocator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WhatExecLib.Abstractions
+{
+    public interface IExecutableFileLocator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="executableName"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<string> LocateExecutableAsync(string executableName, CancellationToken cancellationToken = default);
+
+        Task<bool> IsExecutableInDirectoryAsync(string executableName, string directoryPath,
+            CancellationToken cancellationToken = default);
+    
+        Task<bool> IsExecutableInDriveAsync(string executableName, string driveName, CancellationToken cancellationToken = default);
+
+        //   IEnumerable<string> LocateAllExecutablesWithinFolder(string folder);
+
+        //   IEnumerable<string> LocateAllExecutablesWithinDrive(DriveInfo driveInfo);
+    }
+}
