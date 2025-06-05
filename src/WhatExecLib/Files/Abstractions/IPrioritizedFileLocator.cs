cef0005089f9370b7f99e72306f41e61c30776e5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-05 20:53:42 +0100

    add new file related interfaces

---
+++ src/WhatExecLib/Files/Abstractions/IPrioritizedFileLocator.cs
@@ -0,0 +1,25 @@
+﻿/*
+    WhatExecLib
+    Copyright (c) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Generic;
+using WhatExecLib.Executables;
+using WhatExecLib.Prioritizers;
+
+namespace WhatExecLib.Files.Abstractions
+{
+    public interface IPrioritizedFileLocator : IFileLocator
+    {
+        DirectoryPriority DirectoryPriority { get; }
+        
+        void PrioritizeDirectory(string directory);
+        void PrioritizeDirectories(IEnumerable<string> directories);
+        void PrioritizeDirectories(DirectoryPriority priority, IEnumerable<string> directories);
+
+    }
+}
