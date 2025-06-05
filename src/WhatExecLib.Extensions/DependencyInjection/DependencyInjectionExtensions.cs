29382e177b0b60d79aa72ccb200bbd95a05c6548
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-05 20:59:13 +0100

    add copyright notice

--- src/WhatExecLib.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/WhatExecLib.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -1,6 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿/*
+    WhatExecLib
+    Copyright (c) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using Microsoft.Extensions.DependencyInjection;
+
 using WhatExecLib.Executables;
 using WhatExecLib.Executables.Abstractions;
+
 using WhatExecLib.Prioritizers;
 using WhatExecLib.Prioritizers.Abstractions;
 
