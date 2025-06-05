c846b157c66417c898e1f491d2a728559a4edf22
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-05 20:54:46 +0100

    add missing copyright notice

--- src/WhatExecLib/Files/FileLocator.cs
+++ src/WhatExecLib/Files/FileLocator.cs
@@ -1,5 +1,19 @@
-﻿using System.Threading;
+﻿/*
+    WhatExecLib
+    Copyright (c) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+
 using WhatExecLib.Files.Abstractions;
 
 namespace WhatExecLib.Files
