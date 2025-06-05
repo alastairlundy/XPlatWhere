20e8625b7e778f8786b4e7413454d7a38957c8e4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-05 19:36:19 +0100

    start work on locating files

---
+++ src/WhatExecLib/Files/Abstractions/IFileLocator.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WhatExecLib.Files.Abstractions
+{
+    public interface IFileLocator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="executableName"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<string> LocateFileAsync(string executableName, CancellationToken cancellationToken = default);
+
+        Task<bool> IsFileInDirectoryAsync(string executableName, string directoryPath,
+            CancellationToken cancellationToken = default);
+    
+        Task<bool> IsFileWithinDriveAsync(string executableName, string driveName, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/WhatExecLib/Files/FileLocator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WhatExecLib.Files.Abstractions;
+
+namespace WhatExecLib.Files
+{
+    public class FileLocator : IFileLocator
+    {
+        public async Task<string> LocateFileAsync(string executableName, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> IsFileInDirectoryAsync(string executableName, string directoryPath, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> IsFileWithinDriveAsync(string executableName, string driveName, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
