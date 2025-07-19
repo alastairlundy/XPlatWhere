6362bcb745de79b13cb9cd182e91521219738bbc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-19 15:24:32 +0100

    start implementing Where FindExecutables command

--- src/XPlatWhere.Cli/Commands/FindExecutablesCommand.cs
+++ src/XPlatWhere.Cli/Commands/FindExecutablesCommand.cs
@@ -24,6 +24,61 @@ public class FindExecutablesCommand : ICliCommand
     
     public async Task<int> RunAsync(CommandArguments commandArguments)
     {
+        IRefreshableCachedEnumerable<string> results;
+        
+        if (commandArguments.NumberOfFilesToLookFor > 1)
+        {
+           IEnumerable<string> enumerable = await _executableFileInstancesLocator.LocateExecutableInstancesAsync(commandArguments.SearchPattern);
+           results = new RefreshableCachedEnumerable<string>(enumerable, EnumerableMaterializationMode.Lazy);
+        }
+        else
+        {
+            string result = await _executableFileLocator.LocateExecutableAsync(commandArguments.SearchPattern);
+
+            results = new RefreshableCachedEnumerable<string>([result], EnumerableMaterializationMode.Lazy);
+        }
+
+        if (commandArguments.DisplayExitCodeOnly)
+        {
+            if (results.Any())
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+
+        if (commandArguments.DisplayOutputInQuotationMarks)
+        {
+            results.RefreshCache(results.Select(x => x.Insert(0, '"'.ToString())
+                .Insert(x.Length - 1, '"'.ToString())));
+        }
+
+        if (commandArguments.DisplayFileSizeAndLastModifiedForFiles)
+        {
+            foreach (string result in results)
+            {
+                string actual = Path.GetFullPath(result.Replace('"'.ToString(), string.Empty));
+
+                var file = new FileInfo(actual);
+
+               string fileSize = file.GetFileSizeString();
+                DateTime lastModified = File.GetLastWriteTime(actual);
+
+                string output = $"{result} {fileSize} {lastModified.ToShortDateString()} {lastModified.ToShortTimeString()}";
+                
+                Console.WriteLine(output);
+            }
+        }
+        else
+        {
+            foreach (string result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
         
         return 0;
     }
