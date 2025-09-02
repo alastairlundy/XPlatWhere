808f4cc1920fd67f863fb460add9670a78d713e1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-02 11:10:40 +0100

    Update IExecutableFileLocator.cs

--- src/XPlatWhereLib.Abstractions/Executables/IExecutableFileLocator.cs
+++ src/XPlatWhereLib.Abstractions/Executables/IExecutableFileLocator.cs
@@ -7,7 +7,6 @@
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace AlastairLundy.XPlatWhereLib.Abstractions.Executables;
